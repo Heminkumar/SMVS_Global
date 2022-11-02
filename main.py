07c97406e2c36cb02517701382ab3329b8a7ca77
Heminkumar <heminswa67@gmail.com>
2022-11-02 18:23:11 +0530

    main.py update for logs save with date_time

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ today = datetime.now()
 dir = os.path.join(os.getcwd(),"Logs/Logs_"+today.strftime('%Y_%m_%d_%H_%M'))
 
 # change the directory to testcase for execution
-os.chdir("/home/hemin/PycharmProjects/SMVS_Global/TestCases")
+os.chdir(os.getcwd()+"/TestCases")
 
 # execute robot command and save logs in Logs/ with timestamp
 os.system("robot --outputdir "+ dir +" Test_Login_Page_TCs.robot")
